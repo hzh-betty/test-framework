48000ed5f2bd8f3858281ab6ccabb0e0ac3a5f26
hzh-betty <beidi_99@outlook.com>
2026-04-25 10:56:39 +0800

    refactor(legacy): 删除旧小文件 20

--- framework/reporting/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Reporting adapters."""
-
-from .allure_reporter import AllureReporter, ReportContext
-from .statistics import build_statistics, write_statistics
-
-__all__ = ["AllureReporter", "ReportContext", "build_statistics", "write_statistics"]
